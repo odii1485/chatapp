86c95fc861259241f4a05c306732a80410fb52b7
odii1485 <rahulodedra1485@gmail.com>
2021-11-24 23:45:04 +0530

    chat app

---
+++ app/src/main/java/com/odii/chatapp/otp_authetication.java
@@ -0,0 +1,15 @@
+package com.odii.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otp_authetication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_authetication);
+
+    }
+}
---
+++ app/src/main/java/com/odii/chatapp/set_profile.java
@@ -0,0 +1,14 @@
+package com.odii.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class set_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_profile);
+    }
+}
